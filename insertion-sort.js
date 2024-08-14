481b810790847adaa720df14229a576a930c522b
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-14 23:20:41 +0600

    Fixed the issue about the big-O notation of bubble sort and insertion sort

---
+++ insertion-sort.js
@@ -0,0 +1,29 @@
+const insertionSort = (arr) => {
+  const arrayToSort = [...arr];
+  let sortedElement = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    for (let j = i - 1; j >= 0; j--) {
+      if (arrayToSort[j] < numberToInsert) {
+        arrayToSort[j + 1] = numberToInsert;
+        continue;
+      }
+
+      if (i === 1) {
+        [arrayToSort[j], arrayToSort[j + 1]] = [
+          arrayToSort[j + 1],
+          arrayToSort[j],
+        ];
+      }
+
+      if (arrayToSort[j] > numberToInsert) {
+        arrayToSort[j + 1] = arrayToSort[j];
+      }
+    }
+  }
+  return arrayToSort;
+};
+
+console.log(insertionSort([21, -6, 20, 8, 4, -2]));
+
+// The time complexity is O(n^2) which is a quadratic time complexity.
