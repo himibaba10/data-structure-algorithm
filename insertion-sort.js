813dcbcab5e053949387ba0b98dc7d5ed2d85681
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-15 01:23:10 +0600

    Understood the mentor's procedure for insertion sort

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,3 +1,4 @@
+// My solution
 const insertionSort = (arr) => {
   const arrayToSort = [...arr];
   for (let i = 1; i < arr.length; i++) {
@@ -23,6 +24,20 @@ const insertionSort = (arr) => {
   return arrayToSort;
 };
 
-console.log(insertionSort([21, -6, 20, 8, 4, -2]));
+//Mentor's solution
+const insertionSortMentor = (arr) => {
+  for (let i = 1; i < arr.length; i++) {
+    let numberToInsert = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > numberToInsert) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = numberToInsert;
+  }
+  return arr;
+};
+
+console.log(insertionSortMentor([21, -6, 20, 8, 4, -2]));
 
 // The time complexity is O(n^2) which is a quadratic time complexity.
