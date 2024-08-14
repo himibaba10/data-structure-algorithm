9a36a26e6c7c21133757290cb2ceed9db8894289
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-14 23:11:54 +0600

    Solved the bubble sort algorithm

---
+++ bubble-sort.js
@@ -0,0 +1,16 @@
+const bubbleSort = (arr) => {
+  let swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < arr.length - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+  }
+
+  return arr;
+};
+
+console.log(bubbleSort([6, -2, 4, -10]));
