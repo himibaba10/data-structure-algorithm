e234fb75e6fb77a080d546f5f603fe6322dc946d
Ferdous Ahmed <himibaba10@gmail.com>
2024-08-16 18:58:09 +0600

    Solved the cartesian product algorithm and found the worst time complexity

---
+++ cartesian-product.js
@@ -0,0 +1,13 @@
+const cartesianProduct = (arr1, arr2) => {
+  const result = [];
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      result.push([arr1[i], arr2[j]]);
+    }
+  }
+  return result;
+};
+
+console.log(cartesianProduct([1, 2], [2, 3, 5]));
+
+//The time complexity is bigO = mn
